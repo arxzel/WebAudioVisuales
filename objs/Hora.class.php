60b000c792d3cb64fa3be92586316c0da3c19ef5
arxzel <arxzel@github.com>
2017-10-23 19:22:10 -0500

    Se ha agregado los objetos classes a usar en los modelos

---
+++ objs/Hora.class.php
@@ -0,0 +1,26 @@
+<?php
+class Hora{
+    private $idHora;
+    private $hora;
+
+    public function __construct(){
+
+    }
+
+    public function getIdHora (){
+        return $this->idHora;
+    }
+
+    public function setIdHora($idHora){
+        $this->idHora = $idHora;
+    }
+
+    public function getHora (){
+        return $this->Hora;
+    }
+
+    public function setHora($Hora){
+        $this->Hora = $Hora;
+    }
+}
+ ?>
