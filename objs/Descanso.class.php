52f91a4fa2a69ae7fe2ce4346cdbf1c3eb9f72a1
ferchounisangil <32595734+ferchounisangil@users.noreply.github.com>
2017-10-26 20:03:50 -0500

    Actualización con los objetos
    Se inició a trabajar con los objetos de los usuarios, sólo se modificó el archivo Descanso.php

---
+++ objs/Descanso.class.php
@@ -0,0 +1,25 @@
+<?php
+class Descanso{
+	private $idHorarioDescanso;
+	private $nombre;
+	private $horaInicio;
+	private $duracion;
+	private $estado;
+
+	public fuction construct (){
+
+	}
+
+	public function getIdHorarioDescanso(){
+        return $this->idHorarioDescanso;
+    }
+
+    public function setIdHorarioDescanso($idHorarioDescanso){
+        $this->idHorarioDescanso = $$idHorarioDescanso;
+    }
+
+
+}
+
+
+?>
