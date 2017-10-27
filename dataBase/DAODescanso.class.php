70862c66f692f63e0e84cebff99469a715f4c719
BetoRincon <aluisrincon@unisangil.edu.co>
2017-10-26 19:41:23 -0500

    DAODescanso hecho por beto y parte de DAOParametroHorario hecho por Sergio

--- dataBase/DAODescanso.class.php
+++ dataBase/DAODescanso.class.php
@@ -13,29 +13,46 @@ class DAODescanso extends DataBase{
     }
 
     public function getDescansoById($descanso)
-    {
-    
-        $sql = "SELECT * FROM descansos WHERE id_descanso = " . $descanso->getIdDescanso();
-
-        $resulset = $this->selectQuery($sql);
-
-        $descanso = new Descanso();
-
-        foreach($resulset as $row){
-
-            $descanso -> setIdDescanso((int)$row['id_descanso']);
-            /* Ojo Preguntar por los tipos de datos */
-            $descanso -> setNombre($row['nombre']);
-            $descanso->setHoraInicio($row['hora_inicio']);
-            $descanso->setDuracion($row['duracion']);
-            $descanso->setEstado((int)$row['estado']);
-            
+        {
+             $sql = "SELECT * FROM descansos WHERE id_descanso = " . $descanso->getIdDescanso();
+             $resulset = $this->selectQuery($sql);
+             return $this-> construirDescanso($resulset);
         }
 
-        return $descanso;
-
-
+    private function construirDescanso($resulset){
+        $descanso = new Descanso();
         
+                foreach($resulset as $row){
+        
+                    $descanso -> setIdDescanso((int)$row['id_descanso']);
+                    /* Ojo Preguntar por los tipos de datos */
+                    $descanso -> setNombre($row['nombre']);
+                    $descanso->setHoraInicio($row['hora_inicio']);
+                    $descanso->setDuracion($row['duracion']);
+                    $descanso->setEstado((int)$row['estado']);
+                    
+                }
+        
+                return $descanso;
+    }
+
+    private function construirDescansos(){
+        $descansos = new Array();
+        foreach($resulset as $row)
+                    {
+                        $descanso = new Descanso();
+                        $descanso -> setIdDescanso((int)$row['id_descanso']);
+                        /* Ojo Preguntar por los tipos de datos */
+                        $descanso -> setNombre($row['nombre']);
+                        $descanso->setHoraInicio($row['hora_inicio']);
+                        $descanso->setDuracion($row['duracion']);
+                        $descanso->setEstado((int)$row['estado']);
+                        $descansos[] = $descanso;
+                       
+                        
+                    }
+            
+                    return $descansos;
     }
 
 }
