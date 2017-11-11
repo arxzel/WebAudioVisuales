4bcc54e63b1063e425db047a7c52ac5ca9620e8c
ferchounisangil <luisrodriguez1@unisangil.edu.co>
2017-11-10 22:03:33 -0500

    controlador periodo academico

--- controllers/PeriodoAcademicoController.class.php
+++ controllers/PeriodoAcademicoController.class.php
@@ -4,7 +4,7 @@ $requires = new Requires();
 $requires->importDAOPeriodoAcademico();
 
 
-class PeriodoAcademico
+class PeriodoAcademicoController
 {
     private $daoPeriodoAcademico;
     public function __construct()
@@ -12,5 +12,39 @@ class PeriodoAcademico
         $this->daoPeriodoAcademico = new DAOPeriodoAcademico();
     }
 
-    
+    public function getPeriodoAcademicoById($PeriodoAcademico)
+    {
+    	return $this->daoPeriodoAcademico->getPeriodoAcademicoById($PeriodoAcademico);
+    }
+
+    public function getPeriodoAcademicoByEstado($PeriodoAcademico)
+    {
+    	return $this->daoPeriodoAcademico->getPeriodoAcademicoByEstado($PeriodoAcademico);
+    }
+
+    public function insertPeriodoAcademico($PeriodoAcademico)
+    {
+    	return $this->daoPeriodoAcademico->insertPeriodoAcademico($PeriodoAcademico);
+    }
+
+    public function deletePeriodoAcademico($PeriodoAcademico,$usuario)
+    {
+    	return $this->daoPeriodoAcademico->deletePeriodoAcademico($PeriodoAcademico,$usuario);
+    }
+
+    public function updatePeriodoAcademico($PeriodoAcademico,$usuario)
+    {
+    	return $this->daoPeriodoAcademico->updatePeriodoAcademico($PeriodoAcademico,$usuario);
+    }
+
+    public function construirPeriodoAcademico($resulset)
+    {
+        return $this->daoPeriodoAcademico->construirPeriodoAcademico($resulset);
+    }
+
+    public function construirPeriodosAcademicos($resulset)
+    {
+    	return $this->daoPeriodoAcademico->construirPeriodoAcademico($resulset);
+    }
 }
+?>
